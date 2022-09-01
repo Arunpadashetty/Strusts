f43d18a4fb3af54808d1e52aa869490f61d59f98
Arunpadashetty <111106020+Arunpadashetty@users.noreply.github.com>
2022-09-01 15:59:28 +0530

    Add files via upload

---
+++ ValidationDemo/src/main/java/com/training/model/Student.java
@@ -0,0 +1,12 @@
+package com.training.model;
+
+public class Student {
+	
+	private String name;
+	private String password;
+	private int age;
+	private String gender; 
+	
+	private List<Hobbies>
+
+}
